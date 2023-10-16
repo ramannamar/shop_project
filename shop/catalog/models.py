e1f41db44fd789c4687a1fee03940aef4a7a723a
raman162 <amazingbublik162@gmail.com>
2023-10-16 19:39:56 +0300

    add orders functionality

--- shop/catalog/models.py
+++ shop/catalog/models.py
@@ -61,3 +61,75 @@ class Basket(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     count = models.IntegerField(null=True, blank=True)
+
+
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(null=True, blank=True)
+
+
+class Order(models.Model):
+    DELIVERY_METHODS = (
+        ('Courier', 'Courier'),
+        ('Self-delivery', 'Self-delivery'),
+        ('Post', 'Post')
+    )
+
+    DELIVERY_STATUSES = (
+        ('Delivered', 'Delivered'),
+        ('In process', 'In process'),
+        ('Lost', 'Lost')
+    )
+
+    PAYMENT_METHODS = (
+        ('Card', 'Card'),
+        ('Cash', 'Cash'),
+        ('Card Online', 'Card Online')
+    )
+
+    PAYMENT_STATUSES = (
+        ('Paid', 'Paid'),
+        ('In process', 'In process')
+    )
+
+    DELIVERY_NOTIF_IN_TIME = (
+        (24, 24),
+        (6, 6),
+        (1, 1)
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    promocode = models.ForeignKey(Promocode, null=True, blank=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.SET_NULL)
+    result_price = models.DecimalField(max_digits=15, decimal_places=2)
+
+    delivery_address = models.CharField(max_length=200, null=True, blank=True)
+    delivery_method = models.CharField(choices=DELIVERY_METHODS,
+                                       max_length=15,
+                                       default='Courier')
+    delivery_status = models.CharField(choices=DELIVERY_STATUSES,
+                                       max_length=15,
+                                       default='In process')
+    delivery_notif_in_time = models.IntegerField(choices=DELIVERY_NOTIF_IN_TIME,
+                                                 null=True,
+                                                 default=None)
+    delivery_time = models.DateTimeField()
+
+    payment_method = models.CharField(choices=PAYMENT_METHODS,
+                                      max_length=15,
+                                      default='Card Online')
+    payment_statuses = models.CharField(choices=PAYMENT_STATUSES,
+                                        max_length=15,
+                                        default='In process')
+
+
+class OrderProducts(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField()
+
+
+class Cashback(models.Model):
+    percent = models.IntegerField()
+    treshold = models.IntegerField()
--- shop/users/urls.py
+++ shop/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
-from users. views import ActivateUser, SomeView
+from users. views import ActivateUser
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('accounts/activate/<uid>/<token>/',
-         ActivateUser.as_view({'get': 'activation'}), name='activation'),
-    path('some-view/', SomeView.as_view(), name='some-view')
+         ActivateUser.as_view({'get': 'activation'}), name='activation')
 ]
